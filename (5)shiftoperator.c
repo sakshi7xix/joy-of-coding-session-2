591e57a886242104a9c8f68555aa329766f09663
sakshi7xix <71366883+sakshi7xix@users.noreply.github.com>
2021-10-07 16:21:44 +0530

    Update (5)shiftoperator.c

--- (5)shiftoperator.c
+++ (5)shiftoperator.c
@@ -5,17 +5,17 @@ int main()
     num=num<<1;
     printf("1st left shift is:%d\t",num);
     num=num<<2;
-    printf("1st left shift is:%d\t",num);
+    printf("2nd left shift is:%d\t",num);
     num=num<<3;
-    printf("1st left shift is:%d\t",num);
+    printf("3rd left shift is:%d\t",num);
     printf("\n");
     num=22;
     num=num>>1;
     printf("1st left shift is:%d\t",num);
     num=num>>2;
-    printf("1st left shift is:%d\t",num);
+    printf("2nd left shift is:%d\t",num);
     num=num>>3;
-    printf("1st left shift is:%d\t",num);
+    printf("3rd left shift is:%d\t",num);
 
 }
 
