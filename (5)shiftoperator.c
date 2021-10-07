2660e1e6902df5eed9a23bebebdc2c9a8fe3c095
sakshi7xix <71366883+sakshi7xix@users.noreply.github.com>
2021-10-07 15:51:50 +0530

    Add files via upload

---
+++ (5)shiftoperator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int num=22;
+    num=num<<1;
+    printf("1st left shift is:%d\t",num);
+    num=num<<2;
+    printf("1st left shift is:%d\t",num);
+    num=num<<3;
+    printf("1st left shift is:%d\t",num);
+    printf("\n");
+    num=22;
+    num=num>>1;
+    printf("1st left shift is:%d\t",num);
+    num=num>>2;
+    printf("1st left shift is:%d\t",num);
+    num=num>>3;
+    printf("1st left shift is:%d\t",num);
+
+}
+
